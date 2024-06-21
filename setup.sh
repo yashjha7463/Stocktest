8fd7c438855311d69cc94564c9be24ae81c30ed3
Yash Kumar Jha <88791129+yashjha7463@users.noreply.github.com>
2024-06-21 21:45:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Update package list and install SDL2 libraries
-# sudo apt-get update
+# Update package list
+echo "Updating package list..."
 sudo apt-get update
-sudo apt-get install libsdl2-dev swig
-pip install --upgrade pip setuptool
+
+# Install necessary libraries and development tools
+echo "Installing necessary libraries and development tools..."
+sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev swig build-essential
+
+# Upgrade pip and setuptools
+echo "Upgrading pip and setuptools..."
+pip install --upgrade pip setuptools
+
+# Install project dependencies from requirements.txt
+echo "Installing project dependencies..."
+pip install -r /mount/src/stocktest/requirements.txt
+
+echo "Setup complete."
