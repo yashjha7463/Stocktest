ef132a4d48c0e8d00d47087001a1864d51768f89
Yash Kumar Jha <88791129+yashjha7463@users.noreply.github.com>
2024-06-21 21:32:46 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Update package list and install SDL2 libraries
+sudo apt-get update
+sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+
+# Install Python dependencies
+pip install -r /mount/src/stocktest/requirements.txt
